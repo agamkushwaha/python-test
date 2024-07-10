6f00e20fad22a2c2c674fb1321ed4a0f37e288f3
Agam Kushwaha <mail.agamkushwaha@gmail.com>
2024-07-10 16:44:15 +0530

    second commit

---
+++ 2.py
@@ -0,0 +1,11 @@
+# operators program
+a = int(input("Enter the first number"))
+b = int(input("Enter the second number"))
+# addition
+print("Addition of two numbers is", a+b)
+# subtraction 
+print("Subtraction of two numbers is", a-b)
+# multiplication
+print("Multiplication of two numbers is", a*b)
+# division
+print("Division of two numbers is", a/b)
